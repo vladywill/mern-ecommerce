0423fe44c4fac8cc632aca19a8ef9afc2d214ba9
vladywill <vladyslavmartyniuk13@gmail.com>
2023-11-16 16:00:06 -0300

    Implementacion de manejo de carritos con mongoose

--- src/dao/models/cart.model.js
+++ src/dao/models/cart.model.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
-import { productSchema } from './product.model.js';
 
 const dbConnection = mongoose.createConnection(process.env.MONGO_URI_ECOMMERCE);
 
 const cartCollection = 'carts';
 
 const cartSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     timestamp: { type: Date, default: Date.now },
-    products: [{ type: productSchema, required: true }]
+    products: {
+        type: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'products'
+                }
+            }
+        ],
+        default: []
+    }
+});
+
+cartSchema.pre('findOne', function() {
+    this.populate('products.product');
 });
 
 export const cartModel = dbConnection.model(cartCollection, cartSchema);
