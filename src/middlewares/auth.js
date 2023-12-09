5583766a9c86580ccc55ac13893c61692886d2a1
vladywill <vladyslavmartyniuk13@gmail.com>
2023-12-08 21:10:50 -0300

    Update adminMiddleware redirect status code

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -10,7 +10,7 @@ export const authMiddleware = (req = request, res = response, next) => {
 
 export const adminMiddleware = (req = request, res = response, next) => {
     if (!req.session.user || req.session.user.role !== 'ADMIN_ROLE') {
-        return res.status(401).redirect('/views/products');
+        return res.status(403).redirect('/views/products');
     }
 
     next();
