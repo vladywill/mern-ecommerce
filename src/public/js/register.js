705a19999383d7694017d99b00a41865d7a52101
vladywill <vladyslavmartyniuk13@gmail.com>
2023-12-23 20:59:22 -0300

    Add age field to user model and registration form

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -5,19 +5,18 @@ const onSubmit = async () => {
         const confirmPassword = document.getElementById('confirmPassword').value;
         const name = document.getElementById('name').value;
         const lastName = document.getElementById('lastName').value;
+        const age = document.getElementById('age').value;
     
         const response = await fetch('/api/users/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ email, password, confirmPassword, name, lastName }),
+            body: JSON.stringify({ email, password, confirmPassword, name, lastName, age }),
         });
         const data = await response.json();
         if (data.error) {
-            const error = document.getElementById('error');
-            error.style.display = 'block';
-            error.innerHTML = data.error;
+            displayError(data.error);
         } else {
             window.location.href = '/views/login';
         }
@@ -27,4 +26,10 @@ const onSubmit = async () => {
     catch (error) {
         console.log("error: ", error);
     }
+}
+
+const displayError = (errorMsg) => {
+    const errorEl = document.getElementById('error');
+    errorEl.style.display = 'block';
+    errorEl.innerHTML = errorMsg;
 }
