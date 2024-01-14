8db3444a52d7743816fd6099a785f2c514d4a429
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-14 15:31:37 -0300

    Refactor user router and carts router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import Router from "./router.js";
 import { 
     getCurrentUser,
     loginGithub,
@@ -7,22 +7,15 @@ import {
     registerUser 
 } from "../dao/controllers/user.controller.js";
 import passport from "passport";
-import { generateToken, passportCall } from "../utils.js";
+import { passportCall } from "../utils.js";
 
-const router = Router();
-
-router.post("/register", registerUser);
-
-router.post("/login", passport.authenticate('login', { failureRedirect: '/views/login', session: false }), loginUser);
-
-router.post("/logout", logoutUser);
-
-router.get("/github", passport.authenticate("github", { scope: ["user:email"], session: false }), async (req, res) => {});
-
-router.get("/callbackgithub", passport.authenticate("github", { failureRedirect: "/views/login", session: false }), loginGithub);
-
-router.get("/current", passportCall("jwt"), getCurrentUser);
-
-router.get("*", (req, res) => res.status(404).send("404 - Not Found!"));
-
-export default router;
+export default class UserRouter extends Router {
+    init() {
+        this.post("/register", ["PUBLIC"], registerUser);
+        this.post("/login", ["PUBLIC"], passport.authenticate('login', { failureRedirect: '/views/login', session: false }), loginUser);
+        this.post("/logout", ["USER_ROLE", "ADMIN_ROLE"], logoutUser);
+        this.get("/github", ["PUBLIC"], passport.authenticate("github", { scope: ["user:email"], session: false }), async (req, res) => {});
+        this.get("/callbackgithub", ["PUBLIC"], passport.authenticate("github", { failureRedirect: "/views/login", session: false }), loginGithub);
+        this.get("/current", ["PUBLIC"], passportCall("jwt"), getCurrentUser);
+    }
+}
