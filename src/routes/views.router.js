4261487e49e87849848a8ae13702530b08896016
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-23 16:45:01 -0300

    Refactor role-based access control in routers

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -6,10 +6,10 @@ export default class ViewRouter extends Router {
         this.get("/", ['PUBLIC'], getHomeView);
         this.get("/products", ['PUBLIC'], getProducts);
         this.get("/realtimeproducts", ['ADMIN_ROLE'], getRealTimeProducts);
-        this.get("/cart/:cid", ['USER_ROLE', 'ADMIN_ROLE'], getCartById);
+        this.get("/cart/:cid", ['USER_ROLE'], getCartById);
         this.get("/login", ['PUBLIC'], getLoginView);
         this.get("/register", ['PUBLIC'], getRegisterView);
-        this.get("/chat", ['USER_ROLE', 'ADMIN_ROLE'], getMessages);
+        this.get("/chat", ['USER_ROLE'], getMessages);
 
     }
 }
