048dbe9e8b3fa1532f8e8e394133db43d95eef52
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-29 16:03:59 -0300

    Fix authorization typo and update access level for /current endpoint

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -15,6 +15,6 @@ export default class UserRouter extends Router {
         this.post("/logout", ["USER_ROLE", "ADMIN_ROLE"], logoutUser);
         this.get("/github", ["PUBLIC"], passport.authenticate("github", { scope: ["user:email"], session: false }), async (req, res) => {});
         this.get("/callbackgithub", ["PUBLIC"], passport.authenticate("github", { session: false }), loginGithub);
-        this.get("/current", ["PUBLIC"], getCurrentUser);
+        this.get("/current", ["USER_ROLE", "ADMIN_ROLE"], getCurrentUser);
     }
 }
